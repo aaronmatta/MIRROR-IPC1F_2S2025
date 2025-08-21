1281ba2ddaf046c0654bfbc9d1127a16cd55d1ca
Aarón Matta <gmatta-2021223@kinal.edu.gt>
2025-08-20 22:10:57 -0600

    Se agregaron nuevas funcionalidades.
    - Se hizo la funcionalidad de código único auto incrementable.
    - Eliminar producto
    - Mostrar datos del estudiante

--- Proyecto1/src/org/aaronmatta/system/Principal.java
+++ Proyecto1/src/org/aaronmatta/system/Principal.java
@@ -11,6 +11,7 @@ public class Principal {
     String nombre, categoria;
     double precio;
     int opcion, cantidadStock, codigoUnico;
+    int ultimoCodigo=-1;
         
     String[][] inventario = new String[100][5];
     String[][] ventas = new String[100][4];
@@ -20,7 +21,8 @@ public class Principal {
     
     public static void main(String[] args) {
         Principal programa = new Principal();
-        programa.agregarDatosPrueba();
+        programa.generarEspaciosVacios();
+//        programa.agregarDatosPrueba();
         programa.menu();
         
     }
@@ -90,19 +92,29 @@ public class Principal {
                             case 4:
                                 break;
                             default:
-                                System.out.println("\n[?]    Ingrese una de las opciones válidas.");
+                                System.out.println("\n[?]    INGRESE UNA DE LAS OPCIONES VÁLIDAS.");
                                 break;
                         }
                         
                     }while(opcion!=4);
-                    
+                    break;
                 case 3:
+                    System.out.println("\n+----------------------------------------+");
+                    System.out.println("|           ELIMINAR  PRODUCTO           |");
+                    System.out.println("+----------------------------------------+");
+                    codigoUnico = ingresarEntero("*    Código: ");
+                    eliminarProducto(codigoUnico);
                     break;
                 case 4:
+                    System.out.println("\n+----------------------------------------+");
+                    System.out.println("|            REGISTRAR  VENTA            |");
+                    System.out.println("+----------------------------------------+");
+                    
                     break;
                 case 5:
                     break;
                 case 6:
+                    mostrarDatosEstudiante();
                     break;
                 case 7:
                     break;
@@ -112,10 +124,84 @@ public class Principal {
                     verListadoProductos();
                     break;
                 default:
-                    System.out.println("\n[?]    Ingrese una de las opciones válidas.");
+                    System.out.println("\n[?]    INGRESE UNA DE LAS OPCIONES VÁLIDAS.");
                     break;
             }
-        }while(opcion!=9);
+        }while(opcion!=8);
+        
+    }
+    
+    public void eliminarProducto(int codigo){
+        boolean encontrado = false;
+        for(int fila=0;fila<100;fila++){
+            if(inventario[fila][0].equals(String.valueOf(codigo))){
+                
+                System.out.println("+----------------------------------------+");
+                System.out.println("[✓]    ESTAS SEGURO DE ELIMINAR EL");
+                System.out.println("       PRODUCTO "+inventario[fila][1]+", ");
+                System.out.println("       CON CÓDIGO "+inventario[fila][0]+" ?");
+                System.out.println("[X]    VUELVE A ESCRIBIR SU CODIGO");
+                System.out.print("       PARA CONFIRMAR: ");
+                codigoUnico = ingresarEntero("");
+                
+                if(inventario[fila][0].equals(String.valueOf(codigoUnico))){
+                    
+                    inventario[fila][0] = "-100";
+                    inventario[fila][1] = "VACIO";
+                    inventario[fila][2] = "VACIO";
+                    inventario[fila][3] = "VACIO";
+                    inventario[fila][4] = "VACIO";
+                    
+                    ordenarProductos();
+                    
+                    encontrado=true;
+                    System.out.println("+----------------------------------------+");
+                    System.out.println("[-]    PRODUCTO ELIMINADO EXITOSAMENTE.");
+                    break;
+                }
+                
+                System.out.println("+----------------------------------------+");
+                System.out.println("[X]    ACCION CANCELADA.");
+                return;
+                
+            }
+        }
+        if(!encontrado){
+            System.out.println("+----------------------------------------+");
+            System.out.println("[?]   NO SE HA ENCONTRADO NINGUN PRODUCTO");
+            System.out.println("      CON ESE CÓDIGO.");
+            return;
+        }
+        
+    }
+    
+    public void ordenarProductos(){
+        String[][] nuevoInventario = new String[100][5];
+        int contador = 0;
+        for(int fila=0;fila<100;fila++){
+            if(!(inventario[fila][0].equals("-100"))){
+                nuevoInventario[contador][0] = inventario[fila][0];
+                nuevoInventario[contador][1] = inventario[fila][1];
+                nuevoInventario[contador][2] = inventario[fila][2];
+                nuevoInventario[contador][3] = inventario[fila][3];
+                nuevoInventario[contador][4] = inventario[fila][4];
+                contador++;
+            }
+        }
+        for(int fila=contador;fila<100;fila++){
+            nuevoInventario[fila][0] = "-100";
+            nuevoInventario[fila][1] = "VACIO";
+            nuevoInventario[fila][2] = "VACIO";
+            nuevoInventario[fila][3] = "VACIO";
+            nuevoInventario[fila][4] = "VACIO";
+        }
+        for(int fila=0;fila<100;fila++){
+            inventario[fila][0] = nuevoInventario[fila][0];
+            inventario[fila][1]=nuevoInventario[fila][1];
+            inventario[fila][2]=nuevoInventario[fila][2];
+            inventario[fila][3]=nuevoInventario[fila][3];
+            inventario[fila][4]=nuevoInventario[fila][4];
+        }
         
     }
     
@@ -179,6 +265,21 @@ public class Principal {
     }
 
     public void agregarProducto(String nombre, String categoria, double precio, int cantidadStock){
+        
+        boolean espacioDisponible = false;
+        for(int fila=0;fila<100;fila++){
+            if(inventario[fila][0].equals("-100")){
+                espacioDisponible=true;
+                break;
+            }
+        }
+        
+        if(!espacioDisponible){
+            System.out.println("+----------------------------------------+");
+            System.out.println("[?]   INVENTARIO LLENO.");
+            return;
+        }
+        
         double confirmarPrecio = validarPositivo(1,precio);
         int confirmarStock = (int) validarPositivo(2,cantidadStock);
         codigoUnico = generarCodigoUnico();
@@ -191,7 +292,9 @@ public class Principal {
                     inventario[fila][2] = categoria;
                     inventario[fila][3] = String.valueOf(precio);
                     inventario[fila][4] = String.valueOf(cantidadStock);
-                    System.out.println("[+]    Producto agregado exitosamente.");
+                    System.out.println("+----------------------------------------+");
+                    System.out.println("[+]    PRODUCTO AGREGADO EXITOSAMENTE.");
+                    System.out.println("       CON CÓDIGO: "+codigoUnico);
                     break;
                 }
             }
@@ -200,12 +303,19 @@ public class Principal {
     }
     
     public int generarCodigoUnico(){
-        return 1;
+        return ++ultimoCodigo;
     }
     
-    public int validarCodigoUnico(int codigoUnico){
-        
-        return 1;
+    public void mostrarDatosEstudiante(){
+        System.out.println("\n+----------------------------------------+");
+        System.out.println("|          DATOS DEL ESTUDIANTE          |");
+        System.out.println("+----------------------------------------+");
+        System.out.println("|    Nombre: Gerson Aarón Matta Aguilar  |");
+        System.out.println("|    Carnet: 202403711                   |");
+        System.out.println("|    Curso: Introducción a la            |");
+        System.out.println("|           Programacion y Computacion 1 |");
+        System.out.println("|    Sección: F                          |");
+        System.out.println("+----------------------------------------+");
     }
     
     public double validarPositivo(int mensaje, double numeroIngresado){
@@ -215,13 +325,13 @@ public class Principal {
             
         }else{
             if(mensaje==1){
-                System.out.println("El precio no puede ser negativo.");
+                System.out.println("[?]   EL PRECIO NO PUEDE SER NEGATIVO.");
             }
             if(mensaje==2){
-                System.out.println("La cantidad en Stock no puede ser negativo.");
+                System.out.println("[?]   EL STOCK NO PUEDE SER NEGATIVO.");
             }
             if(mensaje==3){
-                System.out.println("El número no puede ser negativo.");
+                System.out.println("[?]   EL NÚMERO NO PUEDE SER NEGATIVO.");
             }
         }
         return -100;
@@ -238,7 +348,7 @@ public class Principal {
                 num = Integer.parseInt(input);
                 valido = true;
             } catch (NumberFormatException e) {
-                System.out.println("[?]    Error: Debe ingresar un número entero válido. Porfavor ingresar un número válido.");
+                System.out.println("[?]    ERROR: Debe ingresar un número entero válido. Porfavor ingresar un número válido.");
             }
         } while (!valido);
 
@@ -256,7 +366,7 @@ public class Principal {
             if (!texto.isEmpty() && texto.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+")) {
                 valido = true;
             } else {
-                System.out.println("[?]    Error: Solo se permiten letras. Porfavor ingresar un texto válido: ");
+                System.out.println("[?]    ERROR: Solo se permiten letras. Porfavor ingresar un texto válido: ");
             }
         } while (!valido);
 
@@ -274,16 +384,26 @@ public class Principal {
                 decimal = Double.parseDouble(input);
                 valido = true;
             } catch (NumberFormatException e) {
-                System.out.println("[?]    Error: Debe ingresar un decimal válido. Porfavor ingresar un número válido.");
+                System.out.println("[?]    ERROR: Debe ingresar un decimal válido. Porfavor ingresar un número válido.");
             }
         } while (!valido);
 
         return decimal;
     }
     
+    public void generarEspaciosVacios(){
+        for (int fila=0;fila<100;fila++){
+            inventario[fila][0] = "-100";
+            inventario[fila][1] = String.valueOf("VACIO");
+            for (int col=2;col<5;col++){ //Empieza en 2, porque la seccion de ID y Nombre ya la modifico arriba.
+                inventario[fila][col] = "VACIO";
+            }
+        }
+    }
+    
     public void agregarDatosPrueba(){ //FUNCION SOLO PARA TRABAJAR CON PRUEBAS
         
-        for (int fila=0;fila<25;fila++){
+        for (int fila=0;fila<100;fila++){
             inventario[fila][0] = String.valueOf(fila);
             inventario[fila][1] = String.valueOf("Nombre"+(fila));
             for (int col=2;col<5;col++){ //Empieza en 2, porque la seccion de ID y Nombre ya la modifico arriba.
@@ -291,13 +411,13 @@ public class Principal {
             }
         }
         
-        for (int fila=25;fila<100;fila++){
-            inventario[fila][0] = "-100";
-            inventario[fila][1] = String.valueOf("VACIO");
-            for (int col=2;col<5;col++){ //Empieza en 2, porque la seccion de ID y Nombre ya la modifico arriba.
-                inventario[fila][col] = "VACIO";
-            }
-        }
+//        for (int fila=25;fila<100;fila++){
+//            inventario[fila][0] = "-100";
+//            inventario[fila][1] = String.valueOf("VACIO");
+//            for (int col=2;col<5;col++){ //Empieza en 2, porque la seccion de ID y Nombre ya la modifico arriba.
+//                inventario[fila][col] = "VACIO";
+//            }
+//        }
         
         inventario[6][0]="6";
         inventario[6][1]="NombreUnico";
